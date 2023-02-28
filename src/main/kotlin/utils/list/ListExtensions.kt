688cfbbeadfa2cc14ef25a54d6071b10508e14c0
hk4n <hakan@focotech.se>
2023-02-28 20:06:06 +0100

    Add combine with matrix extension

--- src/main/kotlin/utils/list/ListExtensions.kt
+++ src/main/kotlin/utils/list/ListExtensions.kt
@@ -1,5 +1,7 @@
 package utils.list
 
+typealias Matrix2d<T> = List<List<T>>
+
 operator fun <T> List<T>.component6(): T {
     return get(5)
 }
@@ -14,3 +16,23 @@ inline fun <reified T> List<List<T>>.transpose(): List<List<T>> {
         }.toList()
     }.toList()
 }
+
+inline fun <reified T> Matrix2d<T>.combineWith(that: Matrix2d<T>,
+                                               combiner: (row: Int, column: Int, a: Matrix2d<T>, b: Matrix2d<T>) -> T): List<List<T>> {
+    return when {
+        this.isEmpty() and that.isNotEmpty() -> that
+        that.isEmpty() and this.isNotEmpty() -> this
+        this.isEmpty() and that.isEmpty() -> this
+        else -> {
+            val columns = this.first().size
+            val rows = this.size
+
+            Array(rows) { row ->
+                Array(columns) { column ->
+                    combiner(row, column, this ,that)
+                }.toList()
+            }.toList()
+        }
+    }
+}
+
