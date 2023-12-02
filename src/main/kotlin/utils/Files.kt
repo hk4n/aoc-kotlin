fbf09492075f204028107c306022ccde11df708b
hk4n <hakan@focotech.se>
2023-12-02 21:45:41 +0100

    Add support for multi year AoC

--- src/main/kotlin/utils/Files.kt
+++ src/main/kotlin/utils/Files.kt
@@ -5,9 +5,9 @@ import java.nio.file.Paths
 
 class Files {
     companion object {
-        fun read(day: String, filename: String): List<String> {
+        fun read(day: String, filename: String, year: String = "2022"): List<String> {
            val pwd = Paths.get("").toAbsolutePath().toString()
-            val path: String = "$pwd/src/main/kotlin/$day/$filename"
+            val path: String = "$pwd/src/main/kotlin/$year/$day/$filename"
 
             return File(path).useLines { it.toList() }
         }
